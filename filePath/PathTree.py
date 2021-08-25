7e6783bdece05f56b30dc419a39392fee9721693
zhangzhiquan03 <zhangzhiquan03@kuaishou.com>
2021-08-25 11:01:47 +0800

    base path

--- filePath/PathTree.py
+++ filePath/PathTree.py
@@ -1,8 +1,25 @@
 import os
+import os.path
+
+file_path = "/Users/zhangzhiquan/Github/PythonPractice/file.txt"
+dir_path = "/Users/zhangzhiquan/Github/PythonPractice/dir.txt"
+root_path = "./ks-features/ft-live/live/src/main/java/com/yxcorp/gifshow/live"
 normal_symbol = '├── '
 end_symbol = '└── '
-file_filter_list = ['PathTree.py', 'cal.py']
-dir_filter_list = ['../filePath', '../Number']
+file_filter_list = []
+dir_filter_list = []
+
+
+def get_file_list(file_path):
+    file = open(file_path)
+    return file.readlines()
+
+
+def get_filter_files():
+    global file_filter_list
+    global dir_filter_list
+    file_filter_list = get_file_list(file_path)
+    dir_filter_list = get_file_list(dir_path)
 
 
 def get_symbol(file_list, index):
@@ -39,4 +56,6 @@ def dfs_show_dir(path, depth):
 
 
 if __name__ == '__main__':
-    dfs_show_dir('..', 0)
+    os.chdir('/Users/zhangzhiquan/KS/kwai-android')
+    get_filter_files()
+    dfs_show_dir(root_path, 0)
